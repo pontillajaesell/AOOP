392b90f1503d1561a4ab951936efaad87770ceb1
pontillajaesell <pontillajaesell@gmail.com>
2025-03-16 16:42:55 +0800

    Create Concatenate.cs

---
+++ Practices/Concatenate.cs
@@ -0,0 +1,10 @@
+using System;
+
+Console.Write("Enter number 1: ");
+String num1 = Console.ReadLine();
+Console.Write("Enter number 2: ");
+String num2 = Console.ReadLine();
+Console.Write("Enter number 3: ");
+String num3 = Console.ReadLine();
+
+Console.WriteLine($"{num1 + num2 + num3}");
